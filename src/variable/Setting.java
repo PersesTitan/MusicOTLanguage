d6c7a28f825136128b010f5da5ef77d374cdc7c3
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-05 01:10:12 +0900

    add findKey

--- src/variable/Setting.java
+++ src/variable/Setting.java
@@ -5,4 +5,10 @@ import java.util.Map;
 
 public class Setting {
     public static final Map<String, String> var = new HashMap<>();
+
+    public boolean findKey(String key) {
+        return var.containsKey(key);
+    }
+
+
 }
