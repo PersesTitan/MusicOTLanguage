20756135e0bfd0e5391acd711a6c91b9e6bbe3d9
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-05 01:11:15 +0900

    출력 부분 정리

--- src/run/Encode.java
+++ src/run/Encode.java
@@ -2,22 +2,39 @@ package run;
 
 import gui.ShowMusic;
 
-import java.util.Map;
 import java.util.StringTokenizer;
 
-public class Encode {
+public class Encode implements MainRun {
 
     private final ShowMusic showMusic = new ShowMusic();
+    private final StringBuffer buffer = new StringBuffer();
 
     public void start(String line) throws Exception {
         StringTokenizer tokenizer = new StringTokenizer(line);
+        buffer.setLength(0);
         while (tokenizer.hasMoreTokens()) {
             String text = tokenizer.nextToken();
-            showMusic.addLayout(text);
-            LineThread lineThread = new LineThread(text);
-            lineThread.start();
-        }
+            new LineThread(text).start();
 
+            replace(text);
+        }
+        showMusic.setTotalText(buffer.toString());
         Thread.sleep(1000);
+    }
+
+    private void replace(String text) {
+        switch (text) {
+            case sound1 -> buffer.append("도 ");
+            case sound2 -> buffer.append("레 ");
+            case sound3 -> buffer.append("미 ");
+            case sound4 -> buffer.append("파 ");
+            case sound5 -> buffer.append("솔 ");
+            case sound6 -> buffer.append("라 ");
+            case sound7 -> buffer.append("시 ");
+            case drum1 -> buffer.append("드럼1 ");
+            case drum2 -> buffer.append("드럼2 ");
+            case drum3 -> buffer.append("드럼3 ");
+            case drum4 -> buffer.append("드럼4 ");
+        }
     }
 }
