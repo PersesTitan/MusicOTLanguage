b0b2f0e58e089e28f87aaabc22e8d5529331d85f
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-05 01:10:12 +0900

    main test

--- src/Main.java
+++ src/Main.java
@@ -15,28 +15,28 @@ public class Main {
     }
 
     private Main(String[] args) throws Exception {
-        if (args.length < 1) throw new Exception("파일이 존재하지 않습니다.");
-        else if (!new File(args[0]).canRead()) throw new Exception("파일을 읽을 수 없습니다.");
-        else if (!args[0].toLowerCase(Locale.ROOT).endsWith("otlm")) throw new Exception("확장자를 확인해주세요.");
+//        if (args.length < 1) throw new Exception("파일이 존재하지 않습니다.");
+//        else if (!new File(args[0]).canRead()) throw new Exception("파일을 읽을 수 없습니다.");
+//        else if (!args[0].toLowerCase(Locale.ROOT).endsWith("otlm")) throw new Exception("확장자를 확인해주세요.");
 
-//        args = new String[1];
-//        args[0] = "ㅇ도ㅇ ㅇ레ㅇ ㅇ도ㅇ\n" +
-//                "ㅇ도ㅇ\n" +
-//                "\n" +
-//                "ㅇ드ㅇ ㅇ도ㅇ\n" +
-//                "ㅇ드드ㅇ";
-//
-//        Encode encode = new Encode();
-//        for (String line : args[0].split("\\n")) {
-//            encode.start(line);
-//        }
+        args = new String[1];
+        args[0] = """
+                ㅇ도ㅇ ㅇ레ㅇ ㅇ도ㅇ
+                ㅇ도ㅇ
 
-        try (var reader = new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8))) {
-            final Encode encode = new Encode();
-            String text;
-            while ((text = reader.readLine()) != null) encode.start(text);
+                ㅇ드ㅇ ㅇ도ㅇ
+                ㅇ드드ㅇ""";
+
+        Encode encode = new Encode();
+        for (String line : args[0].split("\\n")) {
+            encode.start(line);
         }
-        pause();
+
+//        try (var reader = new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8))) {
+//            final Encode encode = new Encode();
+//            String text;
+//            while ((text = reader.readLine()) != null) encode.start(text);
+//        }
     }
 
     private void pause() {
