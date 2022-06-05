343fad633930a9c1ce1d592f521bef27daf67161
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-06 01:00:54 +0900

    add con

--- src/variable/Setting.java
+++ src/variable/Setting.java
@@ -2,9 +2,11 @@ package variable;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.regex.Pattern;
 
 public class Setting {
     private final String con = "\\n\\s*ㅇMㅇ\\s";
+    private final Pattern pattern = Pattern.compile(con);
     private static final Map<String, String> var = new HashMap<>();
 
     public boolean findKey(String key) {
