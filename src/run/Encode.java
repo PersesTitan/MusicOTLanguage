cdd4668102321c26b1d4c5abebdda1c14b929de2
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-09 07:26:47 +0900

    Thread 추가

--- src/run/Encode.java
+++ src/run/Encode.java
@@ -5,12 +5,14 @@ import java.util.StringTokenizer;
 
 public class Encode {
 
-
     public void start(String line) throws Exception {
         StringTokenizer tokenizer = new StringTokenizer(line);
         while (tokenizer.hasMoreTokens()) {
             String text = tokenizer.nextToken();
-
+            LineThread lineThread = new LineThread(text);
+            lineThread.start();
         }
+
+        Thread.sleep(1000);
     }
 }
