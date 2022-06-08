154322db61c5a41567f744e20713911c52494511
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-09 01:11:48 +0900

    make Thread

--- src/run/LineThread.java
+++ src/run/LineThread.java
@@ -1,9 +1,15 @@
 package run;
 
-public class LineThread extends Thread implements Runnable {
+public class LineThread extends Thread implements Runnable, MainRun {
+
+    protected String line;
+
+    public LineThread(String line) {
+        this.line = line;
+    }
 
     @Override
     public void run() {
-
+        try {mainStart(line);} catch (Exception ignored) {}
     }
 }
