7d9873e1b9d1f1bc8594b268f5c90096f791d0a3
PersesTitan <ToramPersesTitan@gmail.com>
2022-06-09 07:27:01 +0900

    테스트 코드 추가

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
-import run.Drum;
 import run.Encode;
-import run.Piano;
 
 import java.io.BufferedReader;
 import java.io.File;
@@ -8,6 +6,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Locale;
+import java.util.StringTokenizer;
 
 public class Main {
 
@@ -20,8 +19,20 @@ public class Main {
         else if (!new File(args[0]).canRead()) throw new Exception("파일을 읽을 수 없습니다.");
         else if (!args[0].toLowerCase(Locale.ROOT).endsWith("otlm")) throw new Exception("확장자를 확인해주세요.");
 
+//        args = new String[1];
+//        args[0] = "ㅇ도ㅇ ㅇ레ㅇ ㅇ도ㅇ\n" +
+//                "ㅇ도ㅇ\n" +
+//                "\n" +
+//                "ㅇ드ㅇ ㅇ도ㅇ\n" +
+//                "ㅇ드드ㅇ";
+//
+//        Encode encode = new Encode();
+//        for (String line : args[0].split("\\n")) {
+//            encode.start(line);
+//        }
+
         try (var reader = new BufferedReader(new FileReader(args[0], StandardCharsets.UTF_8))) {
-            final var encode = new Encode();
+            final Encode encode = new Encode();
             String text;
             while ((text = reader.readLine()) != null) encode.start(text);
         }
